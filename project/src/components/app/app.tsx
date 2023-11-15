55b91f5d8ca0f356d94bc421eb067574e168d530
koddaks <koss199710@gmail.com>
2023-11-15 21:29:06 +0200

    start to make Routes

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,12 +1,28 @@
-import Main from '../../pages/main/main';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PlaceCardProps } from '../cities-card/place-card';
+import { AppRoute } from '../../routes/routes';
+import Main from '../../pages/main/main';
+import Favorites from '../../pages/favorites/favorites';
+import LogIn from '../../pages/login/login';
+import Property from '../../pages/property/property';
+import Page404 from '../../pages/page404/page404';
 
 export type AppProps = {
   cards: PlaceCardProps[];
 };
 
 function App({ cards }: AppProps): JSX.Element {
-  return <Main cards={cards}></Main>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={AppRoute.Root} element={<Main cards={cards} />}></Route>
+        <Route path={AppRoute.Favorites} element={<Favorites />}></Route>
+        <Route path={AppRoute.Login} element={<LogIn />}></Route>
+        <Route path={AppRoute.Property} element={<Property />}></Route>
+        <Route path="*" element={<Page404 />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
