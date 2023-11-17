04d6235d4a7c28ee47157251378358d3b02ba75c
koddaks <koss199710@gmail.com>
2023-11-18 01:52:23 +0200

    add PlacesSortig component

--- project/src/pages/main/main.tsx
+++ project/src/pages/main/main.tsx
@@ -1,5 +1,6 @@
 import PlacesCardList from '../../components/cities-card-list/place-card-list';
 import Locations from '../../components/locations/locations';
+import PlacesSortig from '../../components/plases-sorting/places-sorting';
 import { Offer } from '../../types';
 
 type MainProps = {
@@ -63,32 +64,7 @@ function Main({ offers }: MainProps): JSX.Element {
               <b className="places__found">
                 {offers.length} places to stay in Amsterdam
               </b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex={0}>
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use href="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li
-                    className="places__option places__option--active"
-                    tabIndex={0}
-                  >
-                    Popular
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: low to high
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Price: high to low
-                  </li>
-                  <li className="places__option" tabIndex={0}>
-                    Top rated first
-                  </li>
-                </ul>
-              </form>
+              <PlacesSortig/>
               <PlacesCardList offers={offers} />
             </section>
             <div className="cities__right-section">
