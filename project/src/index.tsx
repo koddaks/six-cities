e34e990f86812b4e3a4b1edf9ddb7f0b2753db2e
koddaks <koss199710@gmail.com>
2023-11-17 12:31:40 +0200

    fix import

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
-import { placeCardsMock } from './mock/cards';
+import { placeCardsMock } from './mock/offers';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
