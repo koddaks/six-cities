a4ddfddbb43209016744e7b7378480b9405a9985
koddaks <koss199710@gmail.com>
2023-11-18 01:51:59 +0200

    make hook to open/close sort option

---
+++ project/src/components/plases-sorting/places-sorting.tsx
@@ -0,0 +1,45 @@
+import { useState } from 'react';
+
+function PlacesSortig() {
+  const [placesOptionActive, setPlacesOptionActive] = useState(true);
+
+  const handleSetPlacesOptionToggle = () => {
+    setPlacesOptionActive(!placesOptionActive);
+  };
+
+  return (
+    <form className="places__sorting" action="#" method="get">
+      <span className="places__sorting-caption">Sort by</span>
+      <span
+        className="places__sorting-type"
+        tabIndex={0}
+        onClick={handleSetPlacesOptionToggle}
+      >
+        Popular
+        <svg className="places__sorting-arrow" width="7" height="4">
+          <use href="#icon-arrow-select"></use>
+        </svg>
+      </span>
+      <ul
+        className={`places__options places__options--custom ${
+          placesOptionActive ? 'places__options--opened' : ''
+        }`}
+      >
+        <li className="places__option places__option--active" tabIndex={0}>
+          Popular
+        </li>
+        <li className="places__option" tabIndex={0}>
+          Price: low to high
+        </li>
+        <li className="places__option" tabIndex={0}>
+          Price: high to low
+        </li>
+        <li className="places__option" tabIndex={0}>
+          Top rated first
+        </li>
+      </ul>
+    </form>
+  );
+}
+
+export default PlacesSortig;
