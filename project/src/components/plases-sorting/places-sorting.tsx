d5c9b9cb0b45030a83569d66e411d179d393deac
Konstantin Borisov <109829632+koddaks@users.noreply.github.com>
2023-11-20 22:54:26 +0200

    Update project/src/components/plases-sorting/places-sorting.tsx
    rename PlacesSortig to PlacesSorting
    
    Co-authored-by: Igor Belogurov <belogurovigor@gmail.com>

--- project/src/components/plases-sorting/places-sorting.tsx
+++ project/src/components/plases-sorting/places-sorting.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-function PlacesSortig() {
+function PlacesSorting() {
   const [placesOptionActive, setPlacesOptionActive] = useState(false);
 
   const handleSetPlacesOptionToggle = () => {
