81d9ecb73278f5c8e0b4517a44025e68c1ff158a
koddaks <koss199710@gmail.com>
2023-11-24 23:34:30 +0200

    add type Review

--- project/src/types/index.ts
+++ project/src/types/index.ts
@@ -11,6 +11,17 @@ export type Offer = {
   bedrooms?: number;
   maxAdults?: number;
   location: PropertiesLocation;
+  reviews?: Review[] | undefined;
+};
+
+export type Review = {
+  id: number;
+  userId: number;
+  username: string;
+  userPhoto: string;
+  date: Date;
+  rating: number;
+  comment: string;
 };
 
 export type PropertiesLocation = {
