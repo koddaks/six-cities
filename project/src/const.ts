c5fadca2b19cee4268668eb0f8572c75be7ea4cb
koddaks <koss199710@gmail.com>
2023-11-27 20:46:31 +0200

    create PLACE_CARD_CLASS_NAMES_MAP

--- project/src/const.ts
+++ project/src/const.ts
@@ -16,3 +16,18 @@ export const URL_MARKER_DEFAULT =
 
 export const URL_MARKER_CURRENT =
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/demo/interactive-map/main-pin.svg';
+
+export const PLACE_CARD_CLASS_NAMES_MAP = {
+  cities: {
+    article: 'cities__card place-card',
+    imageWrapper: 'cities__image-wrapper place-card__image-wrapper',
+  },
+  nearPlaces: {
+    article: 'near-places__card place-card',
+    imageWrapper: 'near-places__image-wrapper place-card__image-wrapper',
+  },
+  favorites: {
+    article: 'favorites__card place-card',
+    imageWrapper: 'favorites__image-wrapper place-card__image-wrapper',
+  },
+};
