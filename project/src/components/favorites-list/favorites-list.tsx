c6d0e87db0e5981f6f70a7629e22f9859854a37c
koddaks <koss199710@gmail.com>
2023-11-27 20:25:44 +0200

    use PlaceCard component to render FavoritesList

--- project/src/components/favorites-list/favorites-list.tsx
+++ project/src/components/favorites-list/favorites-list.tsx
@@ -1,12 +1,12 @@
-import { Link } from 'react-router-dom';
 import { Offer } from '../../types';
-import { AppRoute } from '../../const';
+import PlaceCard from '../cities-card/place-card';
 
 type FavoritesListProps = {
   favoriteOffers: Offer[];
+  setActiveCard: (id: number | null) => void;
 };
 
-function FavoritesList({ favoriteOffers }: FavoritesListProps) {
+function FavoritesList({ favoriteOffers, setActiveCard }: FavoritesListProps) {
   return (
     <section className="favorites">
       <h1 className="favorites__title">Saved listing</h1>
@@ -20,78 +20,13 @@ function FavoritesList({ favoriteOffers }: FavoritesListProps) {
             </div>
           </div>
           <div className="favorites__places">
-            {favoriteOffers.map((filteredOffer) => (
-              <article
-                key={filteredOffer.id}
-                className="favorites__card place-card"
-              >
-                {filteredOffer.premium && (
-                  <div className="place-card__mark">
-                    <span>Premium</span>
-                  </div>
-                )}
-
-                <div className="favorites__image-wrapper place-card__image-wrapper">
-                  <Link
-                    key={filteredOffer.id}
-                    to={`${AppRoute.Property}/${filteredOffer.id}`}
-                  >
-                    <img
-                      className="place-card__image"
-                      src={filteredOffer.src}
-                      width="150"
-                      height="110"
-                      alt="Place"
-                    />
-                  </Link>
-                </div>
-                <div className="favorites__card-info place-card__info">
-                  <div className="place-card__price-wrapper">
-                    <div className="place-card__price">
-                      <b className="place-card__price-value">
-                        &euro;{filteredOffer.value}
-                      </b>
-                      <span className="place-card__price-text">
-                        &#47;&nbsp;night
-                      </span>
-                    </div>
-                    <button
-                      className={`place-card__bookmark-button button ${
-                        filteredOffer.isFavorite
-                          ? 'place-card__bookmark-button--active'
-                          : ''
-                      }`}
-                      type="button"
-                    >
-                      <svg
-                        className="place-card__bookmark-icon"
-                        width="18"
-                        height="19"
-                      >
-                        <use href="#icon-bookmark"></use>
-                      </svg>
-                      <span className="visually-hidden">In bookmarks</span>
-                    </button>
-                  </div>
-                  <div className="place-card__rating rating">
-                    <div className="place-card__stars rating__stars">
-                      <span style={{ width: `${filteredOffer.rating}%` }} />
-                      <span className="visually-hidden">Rating</span>
-                    </div>
-                  </div>
-                  <h2 className="place-card__name">
-                    <Link
-                      key={filteredOffer.id}
-                      to={`${AppRoute.Property}/${filteredOffer.id}`}
-                    >
-                      {filteredOffer.cardName}
-                    </Link>
-                  </h2>
-                  <p className="place-card__type">
-                    {filteredOffer.housingType}
-                  </p>
-                </div>
-              </article>
+            {favoriteOffers.map((offer) => (
+              <PlaceCard
+                key={offer.id}
+                cardType="favorites"
+                offer={offer}
+                setActiveCard={setActiveCard}
+              />
             ))}
           </div>
         </li>
