e84dd2e63633384162c4d13c59a9097d669ba8e0
koddaks <koss199710@gmail.com>
2023-11-29 14:14:12 +0200

    add newOffer type

--- project/src/types/index.ts
+++ project/src/types/index.ts
@@ -14,6 +14,41 @@ export type Offer = {
   reviews?: Review[] | undefined;
 };
 
+export type NewOffer = {
+  city: {
+    location: {
+      latitude: number;
+      longitude: number;
+      zoom: number;
+    };
+    name: string;
+  };
+  description: string;
+  goods: [string];
+  host: {
+    avatarUrl: string;
+    id: number;
+    isPro: boolean;
+    name: string;
+  };
+  id: number;
+  images: [string];
+  isFavorite: boolean;
+  isPremium: boolean;
+  location: {
+    latitude: number;
+    longitude: number;
+    zoom: number;
+  };
+  bedrooms: number;
+  maxAdults: number;
+  previewImage: string;
+  price: number;
+  rating: number;
+  title: string;
+  type: string;
+};
+
 export type Review = {
   id: number;
   userId: number;
