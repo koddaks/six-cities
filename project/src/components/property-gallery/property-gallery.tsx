d4de33977d68a6d5e538805eeb3f40cd2eef3518
koddaks <koss199710@gmail.com>
2023-12-07 12:55:03 +0200

    fixed naming

--- project/src/components/property-gallery/property-gallery.tsx
+++ project/src/components/property-gallery/property-gallery.tsx
@@ -5,12 +5,12 @@ type PropertyGalleryProps = {
 };
 
 function PropertyGallery({ offer }: PropertyGalleryProps): JSX.Element {
-  const gallaryImages = offer?.images.slice(0, 6);
+  const galleryImages = offer?.images.slice(0, 6);
 
   return (
     <div className="property__gallery-container container">
       <div className="property__gallery">
-        {gallaryImages?.map((image) => (
+        {galleryImages?.map((image) => (
           <div key={image} className="property__image-wrapper">
             <img
               className="property__image"
