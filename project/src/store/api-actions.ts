f638b3fa2a04a418b659dd14346442534baa3349
koddaks <koss199710@gmail.com>
2023-12-10 14:30:55 +0200

    create getOffersAction function

---
+++ project/src/store/api-actions.ts
@@ -0,0 +1,19 @@
+import { AxiosInstance } from 'axios';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { AppDispatch, State } from '../types/state';
+import { Offer } from '../types';
+import { APIRoute } from '../const';
+import { getOffers } from './action';
+
+export const getOffersAction = createAsyncThunk<
+  void,
+  undefined,
+  {
+    dispatch: AppDispatch;
+    state: State;
+    extra: AxiosInstance;
+  }
+>('data/getOffers', async (_arg, { dispatch, extra: api }) => {
+  const { data } = await api.get<Offer[]>(APIRoute.Offers);
+  dispatch(getOffers({ offers: data }));
+});
