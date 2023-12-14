aadc73fe3f74e6a424eef5d56ae80b2f8c6f3064
koddaks <koss199710@gmail.com>
2023-12-14 13:58:20 +0200

    import css

--- project/src/components/error-message/error-message.tsx
+++ project/src/components/error-message/error-message.tsx
@@ -1,9 +1,10 @@
 import { useAppSelector } from '../../hooks';
+import './error-message.css';
 
 function ErrorMessage(): JSX.Element | null {
   const error = useAppSelector((state) => state.error);
 
-  return (error) ? <div className='error-message'>{error}</div> : null;
+  return error ? <div className="error-message">{error}</div> : null;
 }
 
 export default ErrorMessage;
