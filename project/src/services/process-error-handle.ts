8b45b0e9891ee5e123541517a5ee4de3a1ccdd67
koddaks <koss199710@gmail.com>
2023-12-14 21:50:14 +0200

    fix message type

--- project/src/services/process-error-handle.ts
+++ project/src/services/process-error-handle.ts
@@ -2,7 +2,7 @@ import { store } from '../store';
 import { setError } from '../store/action';
 import { clearErrorAction } from '../store/api-actions';
 
-export const processErrorHandle = (message: string): void => {
+export const processErrorHandle = (message: string | null): void => {
   store.dispatch(setError({ error: message }));
   store.dispatch(clearErrorAction());
 };
