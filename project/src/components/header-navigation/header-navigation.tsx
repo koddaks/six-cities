97d42ad8c07fde206b53bfd1f1dfada9d7e57b20
koddaks <koss199710@gmail.com>
2023-12-15 11:04:31 +0200

    create header navigation component

---
+++ project/src/components/header-navigation/header-navigation.tsx
@@ -0,0 +1,59 @@
+import { Link } from 'react-router-dom';
+import { AppRoute, AuthorizationStatus } from '../../const';
+import { useAppDispatch, useAppSelector } from '../../hooks';
+import { logoutAction } from '../../store/api-actions';
+
+function HeaderNavigation() {
+  const authorizationStatus = useAppSelector(
+    (state) => state.authorizationStatus
+  );
+
+  const dispatch = useAppDispatch();
+
+  return (
+    <nav className="header__nav">
+      {authorizationStatus === AuthorizationStatus.Auth ? (
+        <ul className="header__nav-list">
+          <li className="header__nav-item user">
+            <a
+              className="header__nav-link header__nav-link--profile"
+              href="#todo"
+            >
+              <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+              <span className="header__user-name user__name">
+                Oliver.conner@gmail.com
+              </span>
+              <span className="header__favorite-count">3</span>
+            </a>
+          </li>
+          <li className="header__nav-item">
+            <Link
+              className="header__nav-link"
+              onClick={(evt) => {
+                evt.preventDefault();
+                dispatch(logoutAction());
+              }}
+              to={AppRoute.Root}
+            >
+              <span className="header__signout">Sign out</span>
+            </Link>
+          </li>
+        </ul>
+      ) : (
+        <ul className="header__nav-list">
+          <li className="header__nav-item user">
+            <Link
+              to={AppRoute.Login}
+              className="header__nav-link header__nav-link--profile"
+            >
+              <div className="header__avatar-wrapper user__avatar-wrapper"></div>
+              <span className="header__login">Sign in</span>
+            </Link>
+          </li>
+        </ul>
+      )}
+    </nav>
+  );
+}
+
+export default HeaderNavigation;
