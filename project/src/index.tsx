22036f98346cd708c019b5ff6f939c0c689fc0bc
koddaks <koss199710@gmail.com>
2023-12-15 13:31:51 +0200

    fix code

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -7,8 +7,8 @@ import { checkAuthAction, getOffersAction } from './store/api-actions';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-store.dispatch(checkAuthAction());
 store.dispatch(getOffersAction());
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
