8103e84fad6bdc58ecd7882681b08b2fad633f7d
koddaks <koss199710@gmail.com>
2023-12-15 11:04:52 +0200

    import HeaderNavigation

--- project/src/pages/favorites/favorites.tsx
+++ project/src/pages/favorites/favorites.tsx
@@ -1,5 +1,6 @@
 import FavoritesEmpty from '../../components/favorites-empty/favorites-empty';
 import FavoritesList from '../../components/favorites-list/favorites-list';
+import HeaderNavigation from '../../components/header-navigation/header-navigation';
 import { useAppSelector } from '../../hooks';
 
 function Favorites() {
@@ -26,27 +27,7 @@ function Favorites() {
                 />
               </a>
             </div>
-            <nav className="header__nav">
-              <ul className="header__nav-list">
-                <li className="header__nav-item user">
-                  <a
-                    className="header__nav-link header__nav-link--profile"
-                    href="#todo"
-                  >
-                    <div className="header__avatar-wrapper user__avatar-wrapper"></div>
-                    <span className="header__user-name user__name">
-                      Oliver.conner@gmail.com
-                    </span>
-                    <span className="header__favorite-count">3</span>
-                  </a>
-                </li>
-                <li className="header__nav-item">
-                  <a className="header__nav-link" href="#todo">
-                    <span className="header__signout">Sign out</span>
-                  </a>
-                </li>
-              </ul>
-            </nav>
+            <HeaderNavigation />
           </div>
         </div>
       </header>
