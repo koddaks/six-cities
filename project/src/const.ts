58e01060b23bdb6446ceabc2843c99a6ab24f766
koddaks <koss199710@gmail.com>
2023-12-28 11:16:04 +0200

    add REVIEW_RATING

--- project/src/const.ts
+++ project/src/const.ts
@@ -37,3 +37,26 @@ export enum NameSpace {
   App = 'APP',
   User = 'USER',
 }
+
+export const REVIEW_RATING = [
+  {
+    title: 'perfect',
+    value: 5,
+  },
+  {
+    title: 'good',
+    value: 4,
+  },
+  {
+    title: 'not bad',
+    value: 3,
+  },
+  {
+    title: 'badly',
+    value: 2,
+  },
+  {
+    title: 'terribly',
+    value: 1,
+  },
+];
