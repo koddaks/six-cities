912c765c86db6f61c511bce3963acf439d1a64c2
koddaks <koss199710@gmail.com>
2023-12-28 13:13:01 +0200

    use ReviewRating component

--- project/src/components/reviews/reviews.tsx
+++ project/src/components/reviews/reviews.tsx
@@ -7,6 +7,7 @@ import {
 } from '../../store/api-actions';
 import { useParams } from 'react-router-dom';
 import { getReviews } from '../../store/app-data/selectors';
+import ReviewRating from '../review-rating/review-rating';
 
 function Reviews() {
   const [rating, setRating] = useState(0);
@@ -55,97 +56,7 @@ function Reviews() {
         <label className="reviews__label form__label" htmlFor="review">
           Your review
         </label>
-        <div className="reviews__rating-form form__rating">
-          <input
-            onChange={handleRatingChange}
-            className="form__rating-input visually-hidden"
-            name="rating"
-            value="5"
-            id="5-stars"
-            type="radio"
-          />
-          <label
-            htmlFor="5-stars"
-            className="reviews__rating-label form__rating-label"
-            title="perfect"
-          >
-            <svg className="form__star-image" width="37" height="33">
-              <use href="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            onChange={handleRatingChange}
-            className="form__rating-input visually-hidden"
-            name="rating"
-            value="4"
-            id="4-stars"
-            type="radio"
-          />
-          <label
-            htmlFor="4-stars"
-            className="reviews__rating-label form__rating-label"
-            title="good"
-          >
-            <svg className="form__star-image" width="37" height="33">
-              <use href="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            onChange={handleRatingChange}
-            className="form__rating-input visually-hidden"
-            name="rating"
-            value="3"
-            id="3-stars"
-            type="radio"
-          />
-          <label
-            htmlFor="3-stars"
-            className="reviews__rating-label form__rating-label"
-            title="not bad"
-          >
-            <svg className="form__star-image" width="37" height="33">
-              <use href="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            onChange={handleRatingChange}
-            className="form__rating-input visually-hidden"
-            name="rating"
-            value="2"
-            id="2-stars"
-            type="radio"
-          />
-          <label
-            htmlFor="2-stars"
-            className="reviews__rating-label form__rating-label"
-            title="badly"
-          >
-            <svg className="form__star-image" width="37" height="33">
-              <use href="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input
-            onChange={handleRatingChange}
-            className="form__rating-input visually-hidden"
-            name="rating"
-            value="1"
-            id="1-star"
-            type="radio"
-          />
-          <label
-            htmlFor="1-star"
-            className="reviews__rating-label form__rating-label"
-            title="terribly"
-          >
-            <svg className="form__star-image" width="37" height="33">
-              <use href="#icon-star"></use>
-            </svg>
-          </label>
-        </div>
+        <ReviewRating rating={rating} handleRatingChange={handleRatingChange} />
         <textarea
           onChange={handleCommentChange}
           className="reviews__textarea form__textarea"
