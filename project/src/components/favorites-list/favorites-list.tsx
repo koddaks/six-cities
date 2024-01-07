5ac0edadd211109ad295d0d1836812e33aed12c0
koddaks <koss199710@gmail.com>
2024-01-07 19:04:09 +0200

    fix FavoritesList component

--- project/src/components/favorites-list/favorites-list.tsx
+++ project/src/components/favorites-list/favorites-list.tsx
@@ -12,94 +12,50 @@ type FavoritesListProps = {
 function FavoritesList({ favoriteOffers }: FavoritesListProps) {
   const dispatch = useAppDispatch();
 
-  const handleSetFavorite = (IsStatusFavorite: boolean, offerId: number): MouseEventHandler<HTMLButtonElement> => (event) => {
-    event.preventDefault();
+  const handleSetFavorite =
+    (
+      IsStatusFavorite: boolean,
+      offerId: number
+    ): MouseEventHandler<HTMLButtonElement> =>
+      (event) => {
+        event.preventDefault();
+        dispatch(
+          postFavoriteAction([
+            IsStatusFavorite
+              ? FavoriteStatus.Favorite
+              : FavoriteStatus.NotFavorite,
+            offerId,
+          ])
+        );
+      };
 
-    dispatch(postFavoriteAction([ IsStatusFavorite ? FavoriteStatus.Favorite : FavoriteStatus.NotFavorite , offerId]));
-
-  };
+  const citiesOffers = new Set(favoriteOffers.map((offer) => offer.city.name));
 
   return (
     <section className="favorites">
       <h1 className="favorites__title">Saved listing</h1>
       <ul className="favorites__list">
-        <li className="favorites__locations-items">
-          <div className="favorites__locations locations locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="#todo">
-                <span>Amsterdam</span>
-              </a>
-            </div>
-          </div>
-          <div className="favorites__places">
-            {favoriteOffers.map((offer) => (
-              <PlaceCard
-                key={offer.id}
-                cardType="favorites"
-                offer={offer}
-                setFavorite={handleSetFavorite}
-              />
-            ))}
-          </div>
-        </li>
-
-        <li className="favorites__locations-items">
-          <div className="favorites__locations locations locations--current">
-            <div className="locations__item">
-              <a className="locations__item-link" href="#todo">
-                <span>Cologne</span>
-              </a>
-            </div>
-          </div>
-          <div className="favorites__places">
-            <article className="favorites__card place-card">
-              <div className="favorites__image-wrapper place-card__image-wrapper">
-                <a href="#todo">
-                  <img
-                    className="place-card__image"
-                    src="img/apartment-small-04.jpg"
-                    width="150"
-                    height="110"
-                    alt="Place"
-                  />
+        {[...citiesOffers].map((city) => (
+          <li key={city} className="favorites__locations-items">
+            <div className="favorites__locations locations locations--current">
+              <div className="locations__item">
+                <a className="locations__item-link" href="#todo">
+                  <span>{city}</span>
                 </a>
               </div>
-              <div className="favorites__card-info place-card__info">
-                <div className="place-card__price-wrapper">
-                  <div className="place-card__price">
-                    <b className="place-card__price-value">&euro;180</b>
-                    <span className="place-card__price-text">
-                      &#47;&nbsp;night
-                    </span>
-                  </div>
-                  <button
-                    className="place-card__bookmark-button place-card__bookmark-button--active button"
-                    type="button"
-                  >
-                    <svg
-                      className="place-card__bookmark-icon"
-                      width="18"
-                      height="19"
-                    >
-                      <use href="#icon-bookmark"></use>
-                    </svg>
-                    <span className="visually-hidden">In bookmarks</span>
-                  </button>
-                </div>
-                <div className="place-card__rating rating">
-                  <div className="place-card__stars rating__stars">
-                    <span style={{ width: '100%' }}></span>
-                    <span className="visually-hidden">Rating</span>
-                  </div>
-                </div>
-                <h2 className="place-card__name">
-                  <a href="#todo">White castle</a>
-                </h2>
-                <p className="place-card__type">Apartment</p>
-              </div>
-            </article>
-          </div>
-        </li>
+            </div>
+            <div className="favorites__places">
+              {favoriteOffers.map((offer) =>(
+                <PlaceCard
+                  key={offer.id}
+                  cardType="favorites"
+                  offer={offer}
+                  setFavorite={handleSetFavorite}
+                />
+              ))}
+            </div>
+          </li>
+        ))}
       </ul>
     </section>
   );
