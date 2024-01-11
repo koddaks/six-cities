74353c6d4ed297476a7ea29884f237e10f613007
koddaks <koss199710@gmail.com>
2024-01-11 11:38:11 +0200

    make navigate(APIRoute.Login) if i try add to bookmark when i do not sign in

--- project/src/components/place-card-list/place-card-list.tsx
+++ project/src/components/place-card-list/place-card-list.tsx
@@ -1,11 +1,13 @@
 import { MouseEventHandler } from 'react';
-import { FavoriteStatus } from '../../const';
+import { APIRoute, AuthorizationStatus, FavoriteStatus } from '../../const';
 import { useAppDispatch, useAppSelector } from '../../hooks';
 import { postFavoriteAction } from '../../store/api-actions';
 import { getOffers } from '../../store/app-data/selectors';
 import { getCurrentCity, getSortType } from '../../store/app-process/selectors';
 import { sortOffers } from '../../utils/sortOffers';
 import PlaceCard from '../place-card/place-card';
+import { getAuthorizationStatus } from '../../store/user-process/selectors';
+import { useNavigate } from 'react-router-dom';
 
 type PlacesCardListProps = {
   setActiveCard: (id: number | null) => void;
@@ -16,16 +18,19 @@ const PlacesCardList = ({ setActiveCard }: PlacesCardListProps) => {
   const offers = useAppSelector(getOffers);
   const activeCity = useAppSelector(getCurrentCity);
   const sortType = useAppSelector(getSortType);
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
   const offersByActiveCity = offers.filter(
     (offer) => offer.city.name === activeCity.name
   );
+  const navigate = useNavigate();
 
   const sortedOffersBySortType = sortOffers(offersByActiveCity, sortType);
 
   const handleSetFavorite = (IsStatusFavorite: boolean, offerId: number): MouseEventHandler<HTMLButtonElement> => (event) => {
     event.preventDefault();
-
-    dispatch(postFavoriteAction([ !IsStatusFavorite ? FavoriteStatus.Favorite : FavoriteStatus.NotFavorite , offerId]));
+    authorizationStatus === AuthorizationStatus.Auth
+      ? dispatch(postFavoriteAction([!IsStatusFavorite ? FavoriteStatus.Favorite : FavoriteStatus.NotFavorite, offerId]))
+      : navigate(APIRoute.Login);
   };
 
   return (
