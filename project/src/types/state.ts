fb43ef639566ccb94f5d95700be70cd4d8219258
koddaks <koss199710@gmail.com>
2024-01-15 11:39:26 +0200

    returned getIsFavoritesLoading

--- project/src/types/state.ts
+++ project/src/types/state.ts
@@ -3,13 +3,14 @@ import { AuthorizationStatus, SortType } from '../const';
 import { store } from '../store';
 import { UserDataProfile } from './user-data';
 
+
 export type State = ReturnType<typeof store.getState>;
 
 export type AppDispatch = typeof store.dispatch;
 
 export type UserProcess = {
   authorizationStatus: AuthorizationStatus;
-  userData: UserDataProfile;
+  userData : UserDataProfile;
 };
 
 export type AppProcess = {
@@ -22,6 +23,7 @@ export type AppData = {
   offerById: Offer | null;
   offersNearby: Offer[];
   reviews: Review[];
-  favoriteOffers: Offer[];
+  favoriteOffers:Offer[];
   isLoading: boolean;
-};
+  isFavoritesOffersLoading: boolean;
+}
