3f7b123d5cc6a57d53207ce3ecd0abd6e062d9d8
koddaks <koss199710@gmail.com>
2024-01-17 12:11:08 +0200

    fix getIsFavoritesLoading selector

--- project/src/store/app-data/selectors.ts
+++ project/src/store/app-data/selectors.ts
@@ -7,6 +7,6 @@ export const getCurrentOffer = (state: State): Offer | null => state[NameSpace.D
 export const getOffersNearby = (state: State): Offer[] => state[NameSpace.Data].offersNearby;
 export const getReviews = (state: State): Review[] => state[NameSpace.Data].reviews;
 export const getIsLoading = (state: State): boolean => state[NameSpace.Data].isLoading;
-export const getIsFavoritesLoading = (state: State): boolean => state[NameSpace.Data].isLoading;
+export const getIsFavoritesLoading = (state: State): boolean => state[NameSpace.Data].isFavoritesOffersLoading;
 export const getFavorites = (state: State): Offer[] => state[NameSpace.Data].favoriteOffers;
 
