fd6864e18805955f01eb3b4a9d12631ff2e83ee8
koddaks <koss199710@gmail.com>
2024-01-29 13:24:16 +0200

    fix BASE_URL

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -4,7 +4,7 @@ import { getToken } from './token';
 import { ErrorResponse } from '../const';
 import { toast } from 'react-toastify';
 
-const BASE_URL = 'https://11.react.pages.academy/six-cities';
+const BASE_URL = 'https://14.design.htmlacademy.pro/six-cities';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
