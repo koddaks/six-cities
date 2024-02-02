a2909bf6d65e057d7ae084103fca67a87bf31f21
koddaks <koss199710@gmail.com>
2024-02-02 11:02:26 +0200

    make show more button

--- project/src/components/reviews-list/reviews-list.tsx
+++ project/src/components/reviews-list/reviews-list.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Review } from '../../types';
 import ReviewsItem from '../reviews-item/reviews-item';
 
@@ -7,14 +7,63 @@ type ReviewsListProps = {
 };
 
 function ReviewsList({ reviews }: ReviewsListProps) {
+  const [visibleItems, setVisibleItems] = useState(10);
+  const [filteredReviews, setFilteredReviews] = useState<Review[]>([]);
+
+  const handleLoadMore = () => {
+    setVisibleItems((prevVisibleItems) => prevVisibleItems + 3);
+  };
+  useEffect(() => {
+    if (Array.isArray(reviews)) {
+      const sortedReviews = reviews.slice().sort((a, b) => new Date(b.date).getTime() - new Date(a.date).getTime());
+      setFilteredReviews(sortedReviews.slice(0, visibleItems));
+    } else {
+      setFilteredReviews([]);
+    }
+  }, [reviews, visibleItems]);
+
+  const hasMoreReviews =
+    Array.isArray(reviews) && visibleItems < reviews.length;
+
   return (
-    <ul className="reviews__list">
-      {Array.isArray(reviews)
-        ? reviews.map((review) => (
-          <ReviewsItem key={review.id} review={review} />
-        ))
-        : null}
-    </ul>
+    <>
+      <ul className="reviews__list">
+        {Array.isArray(filteredReviews)
+          ? filteredReviews.map((review) => (
+              <ReviewsItem key={review.id} review={review} />
+            ))
+          : null}
+      </ul>
+      {hasMoreReviews && (
+        <button
+          className="near-places__button"
+          style={{ width: '30%', display: 'flex', minWidth: '200px' }}
+          onClick={handleLoadMore}
+        >
+          <svg
+            height="20px"
+            width="800px"
+            version="1.1"
+            id="Layer_1"
+            xmlns="http://www.w3.org/2000/svg"
+            xmlnsXlink="http://www.w3.org/1999/xlink"
+            viewBox="0 0 483.049 483.049"
+            xmlSpace="preserve"
+          >
+            <g>
+              <polygon
+                style={{ fill: '#005ECE;' }}
+                points="0,121.155 241.524,361.894 241.524,121.155 	"
+              />
+              <polygon
+                style={{ fill: '#2488FF;' }}
+                points="241.524,121.155 241.524,361.894 483.049,121.155 	"
+              />
+            </g>
+          </svg>
+        </button>
+      )}
+    </>
   );
 }
 
